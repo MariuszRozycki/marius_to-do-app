5c09afb47b6af03de01675ccba9c09f83371c568
MariuszRozycki <mariansc83@icloud.com>
2022-07-13 10:57:43 +0200

    feat: add inputTextHandler & submitToDoHandler

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import './App.css';
 import Form from './components/Form';
+import ToDoList from './components/ToDoList';
 
 function App() {
   return (
     <div className="App">
       <h1>Mariusz's To Do App</h1>
       <Form />
+      <ToDoList />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Form() {
+  const [inputText, setInputText] = useState("");
+  const [toDo, setToDo] = useState([]);
+  console.log("toDo", toDo);
+
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value)
+  }
+
+  const submitToDoHandler = (e) => {
+    e.preventDefault();
+    setToDo([
+      ...toDo,
+      {
+        text: inputText,
+        completed: false,
+        id: Math.random() * 1000
+      }
+    ]);
+    setInputText("");
+  }
+
   return (
     <form>
-      <input type="text" className="to-do--input" />
-      <button className="to-do--button" type="submit">Add task</button>
+      <input value={inputText} type="text" className="to-do--input" onChange={inputTextHandler} />
+      <button className="to-do--button" type="submit" onClick={submitToDoHandler}>Add task</button>
       <div className="select">
         <select name="to-do--options" id="to-do--options" className="filter-options">
           <option value="all">All</option>
