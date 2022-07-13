c893f38451c19359c6d360fb88fc8e26d561d9a4
MariuszRozycki <mariansc83@icloud.com>
2022-07-13 06:26:31 +0200

    feat: add return ToDoList.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
 import './App.css';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
       <h1>Mariusz's To Do App</h1>
+      <Form />
     </div>
   );
 }
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -2,7 +2,9 @@ import React from "react";
 
 function ToDoList() {
   return (
-    <div>ToDoList</div>
+    <div className="to-do--container">
+      <ul className="to-do--list"></ul>
+    </div>
   )
 }
 
